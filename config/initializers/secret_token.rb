89fd0f90143ce24612cc28a51b3762e4c9c07dd4
Steve Tipton <steve.tipton@employmentguide.com>
2010-12-03 13:25:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tzitter::Application.config.secret_token = 'b72cf3789c66592968d41b17d7c51d546bf1292832ea546a7a672f2b14c26f86cb07732c48e6c1e8feaa8abac0a865e7f7ed23c17e5b04d33c77fa35fd04a513'
